3f21093ec461065d248621e3949b72852805404c
Patrick Murimi <kabugap02@gmail.com>
2024-01-03 00:00:02 +0300

    feat: Add new component

---
+++ src/app/modules/blogs/blogs-list/blogs-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blogs-list',
+  templateUrl: './blogs-list.component.html',
+  styleUrl: './blogs-list.component.scss'
+})
+export class BlogsListComponent {
+
+}
