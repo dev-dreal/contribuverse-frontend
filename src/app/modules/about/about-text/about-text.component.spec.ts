c95ee965709eac28418d9415fd0ae64bf4c5a0d5
Patrick Murimi <kabugap02@gmail.com>
2024-01-06 00:20:33 +0300

    feat: About page structure

---
+++ src/app/modules/about/about-text/about-text.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AboutTextComponent } from './about-text.component';
+
+describe('AboutTextComponent', () => {
+  let component: AboutTextComponent;
+  let fixture: ComponentFixture<AboutTextComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AboutTextComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(AboutTextComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
