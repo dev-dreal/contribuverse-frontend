70acc3c3796bf4151125c03ca2f80661d285f9cb
Patrick Murimi <kabugap02@gmail.com>
2024-01-05 04:28:04 +0300

    feat: Smooth transition in initial loading

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -33,12 +33,4 @@ import { InitialLoaderComponent } from '../../shared/components/initial-loader/i
     ]),
   ],
 })
-export class HomeComponent {
-  loading = true;
-
-  ngOnInit() {
-    setTimeout(() => {
-      this.loading = false;
-    }, 5000);
-  }
-}
+export class HomeComponent {}
