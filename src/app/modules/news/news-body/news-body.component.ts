fcb172baa3c13764478e856e28f23391faf3544a
Patrick Murimi <kabugap02@gmail.com>
2024-01-18 05:49:27 +0300

    feat: circular motion in news page

--- src/app/modules/news/news-body/news-body.component.ts
+++ src/app/modules/news/news-body/news-body.component.ts
@@ -1,10 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import {
-  fadingAnimation,
-  slideLeftRightAnimation,
-} from '../../../helpers/animations';
+import { circularMotion, fadingAnimation } from '../../../helpers/animations';
 import { CoolTechAnimalModel } from '../../../models/coolTechAnimal.model';
+import { GlobalsService } from '../../../services/globals/globals.service';
 
 @Component({
   selector: 'news-body',
@@ -12,7 +10,7 @@ import { CoolTechAnimalModel } from '../../../models/coolTechAnimal.model';
   imports: [CommonModule],
   templateUrl: './news-body.component.html',
   styleUrl: './news-body.component.scss',
-  animations: [fadingAnimation, slideLeftRightAnimation],
+  animations: [fadingAnimation, circularMotion],
 })
 export class NewsBodyComponent {
   @Input() activeSlideIndex: number = 0;
@@ -23,18 +21,23 @@ export class NewsBodyComponent {
     {
       title: 'Cool Tech Leopard',
       imgUrl: 'assets/svgs/cool-tech-leopard.svg',
+      state: 'top',
     },
     {
       title: 'Cool Tech Lion',
       imgUrl: 'assets/svgs/cool-tech-lion.svg',
+      state: 'center',
     },
     {
       title: 'Cool Tech Dog',
       imgUrl: 'assets/svgs/cool-tech-dog.svg',
+      state: 'bottom',
     },
   ];
 
   selectedAnimal = this.coolTechAnimals[0];
+
+  constructor(private globals: GlobalsService) {}
 
   ngOnInit() {
     this.currentSlidePositionEvent.emit(this.activeSlideIndex);
@@ -42,6 +45,19 @@ export class NewsBodyComponent {
 
   selectAnimal(index: number) {
     this.activeSlideIndex = index;
+    this.animate(index);
     this.currentSlidePositionEvent.emit(index);
+  }
+
+  animate(clickedIndex: number) {
+    for (let i = 0; i < this.coolTechAnimals.length; i++) {
+      if (i < clickedIndex) {
+        this.coolTechAnimals[i].state = 'top';
+      } else if (i === clickedIndex) {
+        this.coolTechAnimals[i].state = 'center';
+      } else {
+        this.coolTechAnimals[i].state = 'bottom';
+      }
+    }
   }
 }
