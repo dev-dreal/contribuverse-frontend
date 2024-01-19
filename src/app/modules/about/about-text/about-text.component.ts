6226251d878d0b83e1053e13c1cb193bdf917eed
Patrick Murimi <kabugap02@gmail.com>
2024-01-20 01:56:31 +0300

    chore: update font size

--- src/app/modules/about/about-text/about-text.component.ts
+++ src/app/modules/about/about-text/about-text.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-about-text',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './about-text.component.html',
-  styleUrl: './about-text.component.scss'
+  styleUrl: './about-text.component.scss',
 })
-export class AboutTextComponent {
-
-}
+export class AboutTextComponent {}
