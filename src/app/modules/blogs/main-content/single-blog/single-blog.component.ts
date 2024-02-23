79348145933adc604a23aa0cf7a57b9e3425df86
Patrick Murimi <kabugap02@gmail.com>
2024-02-23 12:33:38 +0300

    feat: Single blog works

--- src/app/modules/blogs/main-content/single-blog/single-blog.component.ts
+++ src/app/modules/blogs/main-content/single-blog/single-blog.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import { GET_BLOG } from '../../../../graphql/queries';
+import { BlogModel } from '../../../../models/blog.model';
+import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { fadingAnimation } from '../../../../helpers/animations';
 
 @Component({
   selector: 'single-blog',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './single-blog.component.html',
   styleUrl: './single-blog.component.scss',
+  animations: [fadingAnimation],
 })
-export class SingleBlogComponent {}
+export class SingleBlogComponent {
+  blog: BlogModel = {} as BlogModel;
+  id: string = '';
+  constructor(private apollo: Apollo, private route: ActivatedRoute) {
+    this.id = this.route.snapshot.paramMap.get('id') || '';
+  }
+
+  ngOnInit(): void {
+    this.loadBlog();
+  }
+
+  loadBlog() {
+    this.apollo
+      .watchQuery({
+        query: GET_BLOG,
+        variables: {
+          id: this.id,
+        },
+      })
+      .valueChanges.subscribe(({ data, error }: any) => {
+        console.log(data);
+        this.blog = data.blog;
+        if (error) {
+          console.error(error);
+        }
+      });
+  }
+}
