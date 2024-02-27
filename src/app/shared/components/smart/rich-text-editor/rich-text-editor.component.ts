3850cd62abaa1292bfbfb46e178e02e773b4bcb7
Patrick Murimi <kabugap02@gmail.com>
2024-02-27 09:46:33 +0300

    chore: text-gradient

--- src/app/shared/components/smart/rich-text-editor/rich-text-editor.component.ts
+++ src/app/shared/components/smart/rich-text-editor/rich-text-editor.component.ts
@@ -22,13 +22,13 @@ export class RichTextEditorComponent {
 
 export const QuillConfiguration = {
   toolbar: [
-    ['bold', 'italic', 'underline', 'strike'],
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    ['bold', 'italic', 'underline'],
     ['blockquote', 'code-block'],
     [{ list: 'ordered' }, { list: 'bullet' }],
-    [{ header: [1, 2, 3, 4, 5, 6, false] }],
-    [{ color: [] }, { background: [] }, { align: [] }, { font: [] }],
+    [{ color: [] }, { background: [] }, { align: [] }],
     ['link'],
-    ['image', 'video'],
+    ['image'],
     ['clean'],
   ],
 };
