6a1a1763c44b5116037d9a81b98bb9644f76d390
Patrick Murimi <kabugap02@gmail.com>
2024-02-27 12:41:39 +0300

    feat(auth): Init auth

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,12 +7,21 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { AppRoutes } from './app.routing';
-import { CommonModule } from '@angular/common';
 import { graphqlProvider } from './graphql.provider';
+import { AuthModule } from '@auth0/auth0-angular';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(BrowserModule, CommonModule),
+    importProvidersFrom(
+      BrowserModule,
+      AuthModule.forRoot({
+        domain: 'dev-4zjt23ksc2ktnhc6.us.auth0.com',
+        clientId: 'uN9UfU1dx5DZea1J88hRaOAMDXKdgsL4',
+        authorizationParams: {
+          redirect_uri: window.location.origin,
+        },
+      }),
+    ),
     provideAnimations(),
     provideHttpClient(withInterceptorsFromDi()),
     provideRouter(AppRoutes),
