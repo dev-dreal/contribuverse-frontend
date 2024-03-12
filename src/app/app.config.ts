2a119b88c41c78006251c50f614372f36a754b41
Patrick Murimi <kabugap02@gmail.com>
2024-03-12 03:10:09 +0300

    fix: update environments route

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -12,7 +12,7 @@ import { provideToastr } from 'ngx-toastr';
 import { toastOptions } from './services/toaster/toaster.service';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
-import { environment } from '../environments/environment.development';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
