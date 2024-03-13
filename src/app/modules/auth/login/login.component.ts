fbd179696bbfec8a9228c8c3138c61770d7e5708
Patrick Murimi <kabugap02@gmail.com>
2024-03-13 13:54:09 +0300

    chore: clean up unneccessary files

--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -1,17 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import {
-  FormGroup,
-  FormBuilder,
-  Validators,
-  ReactiveFormsModule,
-} from '@angular/forms';
+import { FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
-import { SupabaseService } from '../../../services/auth/supabase.service';
 import { GlobalsService } from '../../../services/globals/globals.service';
 import { fadingAnimation } from '../../../helpers/animations';
 import { NgxUiLoaderModule, SPINNER } from 'ngx-ui-loader';
-import { AuthSession } from '@supabase/supabase-js';
 import { FirebaseService } from '../../../services/auth/firebase.service';
 
 @Component({
@@ -27,7 +20,6 @@ export class LoginComponent {
   loginForm = {} as FormGroup;
 
   constructor(
-    private supabase: SupabaseService,
     private globals: GlobalsService,
     private firebaseService: FirebaseService,
   ) {}
@@ -42,7 +34,6 @@ export class LoginComponent {
   }
 
   async checkSession() {
-    // const { session } = await this.supabase.getSession();
     if (this.firebaseService.currentUserSig()) {
       this.globals.toast.info('You are already logged in.');
       const redirectUrl =
@@ -78,29 +69,6 @@ export class LoginComponent {
           this.globals.loader.stopAll();
         },
       });
-
-    // this.supabase
-    //   .signInWithPassword(
-    //     this.loginForm.value.email,
-    //     this.loginForm.value.password,
-    //   )
-    //   .then((res) => {
-    //     // Redirect to a specific page after successful login
-    //     const redirectUrl =
-    //       this.globals.route.snapshot.queryParams['redirect_url'];
-    //     if (redirectUrl) {
-    //       this.globals.router.navigateByUrl(redirectUrl);
-    //     } else {
-    //       // Redirect to a default route or specific page if no redirect URL is provided
-    //       this.globals.router.navigate(['/user/profile']);
-    //     }
-    //     this.globals.toast.success('Login successful!');
-    //     this.globals.loader.stopAll();
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //     this.globals.loader.stopAll();
-    //   });
   }
 
   loginWithGoogle() {
@@ -141,25 +109,6 @@ export class LoginComponent {
         this.globals.loader.stopAll();
       },
     });
-  }
-
-  async signInWithGitHub() {
-    await this.supabase.signInWithGithub();
-  }
-
-  logOut() {
-    this.globals.loader.start();
-    this.supabase
-      .signOut()
-      .then(() => {
-        this.globals.toast.success('You have been logged out.');
-        this.globals.loader.stopAll();
-        this.globals.router.navigate(['/']);
-      })
-      .catch((err) => {
-        console.log(err);
-        this.globals.loader.stopAll();
-      });
   }
 
   handleError(controlName: string, errorName: string) {
--- src/app/modules/user-dashboard/profile/profile.component.ts
+++ src/app/modules/user-dashboard/profile/profile.component.ts
@@ -1,12 +1,6 @@
-import { Component, Input } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
-import { AuthSession, User } from '@supabase/supabase-js';
-import {
-  Profile,
-  SupabaseService,
-} from '../../../services/auth/supabase.service';
+import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { AvatarComponent } from '../avatar/avatar.component';
 import { GlobalsService } from '../../../services/globals/globals.service';
 import { FirebaseService } from '../../../services/auth/firebase.service';
 import { NgxUiLoaderModule, SPINNER } from 'ngx-ui-loader';
@@ -14,107 +8,20 @@ import { NgxUiLoaderModule, SPINNER } from 'ngx-ui-loader';
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [
-    CommonModule,
-    AvatarComponent,
-    ReactiveFormsModule,
-    NgxUiLoaderModule,
-  ],
+  imports: [CommonModule, ReactiveFormsModule, NgxUiLoaderModule],
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent {
   SPINNER = SPINNER;
   loading: boolean = false;
-  profile: Profile = {} as Profile;
-  session: AuthSession | null = null;
-
-  updateProfileForm = this.globals.fb.group({
-    username: '',
-    website: '',
-    avatar_url: '',
-  });
 
   constructor(
-    // private readonly supabase: SupabaseService,
     private globals: GlobalsService,
-    protected firebaseService: FirebaseService,
+    protected readonly firebaseService: FirebaseService,
   ) {}
 
-  async ngOnInit(): Promise<void> {
-    // const { session } = await this.supabase.getSession();
-    // this.session = session;
-    // await this.getProfile();
-    // const { username, website, avatar_url } = this.profile;
-    // this.updateProfileForm.patchValue({
-    //   username,
-    //   website,
-    //   avatar_url,
-    // });
-  }
-
-  get avatarUrl() {
-    return this.updateProfileForm.value.avatar_url as string;
-  }
-
-  // async updateAvatar(event: string): Promise<void> {
-  //   this.updateProfileForm.patchValue({
-  //     avatar_url: event,
-  //   });
-  //   await this.updateProfile();
-  // }
-
-  // async getProfile() {
-  //   try {
-  //     this.loading = true;
-  //     const { user } = this.session as { user: User };
-
-  //     const {
-  //       data: profile,
-  //       error,
-  //       status,
-  //     } = await this.supabase.profile(user);
-
-  //     if (error && status !== 406) {
-  //       throw error;
-  //     }
-
-  //     if (profile) {
-  //       this.profile = profile;
-  //     }
-  //   } catch (error) {
-  //     if (error instanceof Error) {
-  //       alert(error.message);
-  //     }
-  //   } finally {
-  //     this.loading = false;
-  //   }
-  // }
-
-  // async updateProfile(): Promise<void> {
-  //   try {
-  //     this.loading = true;
-  //     const { user } = this.session as { user: User };
-
-  //     const username = this.updateProfileForm.value.username as string;
-  //     const website = this.updateProfileForm.value.website as string;
-  //     const avatar_url = this.updateProfileForm.value.avatar_url as string;
-
-  //     const { error } = await this.supabase.updateProfile({
-  //       id: user.id,
-  //       username,
-  //       website,
-  //       avatar_url,
-  //     });
-  //     if (error) throw error;
-  //   } catch (error) {
-  //     if (error instanceof Error) {
-  //       alert(error.message);
-  //     }
-  //   } finally {
-  //     this.loading = false;
-  //   }
-  // }
+  async ngOnInit(): Promise<void> {}
 
   signOut() {
     this.globals.loader.start();
@@ -124,6 +31,5 @@ export class ProfileComponent {
       this.globals.router.navigate(['/']);
       this.globals.loader.stopAll();
     }, 500);
-    // await this.supabase.signOut();
   }
 }
