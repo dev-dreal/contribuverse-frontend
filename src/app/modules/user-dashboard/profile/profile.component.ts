11282881fd652aaef2feff06421d763fbcec5952
Patrick Murimi <kabugap02@gmail.com>
2024-03-13 14:34:09 +0300

    fix: Logged in users can add blogs

--- src/app/modules/user-dashboard/profile/profile.component.ts
+++ src/app/modules/user-dashboard/profile/profile.component.ts
@@ -3,17 +3,15 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { GlobalsService } from '../../../services/globals/globals.service';
 import { FirebaseService } from '../../../services/auth/firebase.service';
-import { NgxUiLoaderModule, SPINNER } from 'ngx-ui-loader';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, NgxUiLoaderModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent {
-  SPINNER = SPINNER;
   loading: boolean = false;
 
   constructor(
@@ -24,12 +22,8 @@ export class ProfileComponent {
   async ngOnInit(): Promise<void> {}
 
   signOut() {
-    this.globals.loader.start();
-    setTimeout(() => {
-      this.firebaseService.logout();
-      this.globals.toast.success('Logged out successfully');
-      this.globals.router.navigate(['/']);
-      this.globals.loader.stopAll();
-    }, 500);
+    this.firebaseService.logout();
+    this.globals.toast.success('Logged out successfully');
+    this.globals.router.navigate(['/home']);
   }
 }
