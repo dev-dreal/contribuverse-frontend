58e50219fa3193fb5ec774025490a14a19f06778
Patrick Murimi <kabugap02@gmail.com>
2024-05-15 19:19:14 +0300

    chore: add status bar to news

--- src/app/modules/news/news-header/news-header.component.ts
+++ src/app/modules/news/news-header/news-header.component.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, inject, input } from '@angular/core';
 import { NavLinkComponent } from '../../../shared/components/ui/nav-link/nav-link.component';
 import { RouterLink } from '@angular/router';
+import { GlobalsService } from '../../../services/globals/globals.service';
 
 @Component({
   selector: 'news-header',
@@ -12,6 +13,12 @@ import { RouterLink } from '@angular/router';
 export class NewsHeaderComponent {
   @Output() isNavLinkClicked = new EventEmitter<boolean>();
 
+  selectedActiveRouteIndex = input.required<number>();
+
+  // DEPENDENCY INJECTION
+  private globals = inject(GlobalsService);
+  // END OF DEPENDENCY INJECTION
+
   navLinks = [
     { label: 'Robotics', route: 'robotics' },
     { label: 'Metaverse', route: 'metaverse' },
@@ -21,5 +28,9 @@ export class NewsHeaderComponent {
 
   onNavClicked() {
     this.isNavLinkClicked.emit(true);
+    this.globals.route.paramMap.subscribe((params) => {
+      console.log(params);
+    });
+    console.log(window.location.pathname);
   }
 }
