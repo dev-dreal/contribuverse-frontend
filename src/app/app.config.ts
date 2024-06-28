88de92dc1f03dbc91ef9d806e3233ddf1f8ccf05
Patrick Murimi <kabugap02@gmail.com>
2024-06-28 03:55:55 +0300

    bug: cant run ssr

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,15 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import {
+  ApplicationConfig,
+  PLATFORM_ID,
+  importProvidersFrom,
+  inject,
+} from '@angular/core';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
 import {
   PreloadAllModules,
   provideRouter,
@@ -13,16 +21,37 @@ import { AppRoutes } from './app.routing';
 import { graphqlProvider } from './graphql.provider';
 import { provideToastr } from 'ngx-toastr';
 import { toastOptions } from './services/toaster/toaster.service';
-import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import {
+  provideFirebaseApp,
+  initializeApp,
+  FirebaseApp,
+  initializeServerApp,
+} from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { environment } from '../environments/environment';
 import { authInterceptor } from './helpers/interceptor';
+import { isPlatformBrowser } from '@angular/common';
+import { release } from 'os';
+import { Request } from 'undici';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     importProvidersFrom([BrowserModule]),
-    provideAuth(() => getAuth()),
-    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth(inject(FirebaseApp))),
+    provideFirebaseApp(() => {
+      if (isPlatformBrowser(inject(PLATFORM_ID))) {
+        return initializeApp(environment.firebaseConfig);
+      }
+      const request = inject(Request, { optional: true });
+      const authIdToken = request?.headers
+        .get('Authorization')
+        ?.split('Bearer ')[1];
+
+      return initializeServerApp(environment.firebaseConfig, {
+        authIdToken,
+        releaseOnDeref: request || undefined,
+      });
+    }),
     provideAnimations(),
     provideHttpClient(withInterceptors([authInterceptor])),
     provideRouter(
@@ -32,6 +61,7 @@ export const appConfig: ApplicationConfig = {
       withDebugTracing(),
     ),
     provideToastr(toastOptions),
-    graphqlProvider, provideClientHydration(),
+    graphqlProvider,
+    provideClientHydration(),
   ],
 };
